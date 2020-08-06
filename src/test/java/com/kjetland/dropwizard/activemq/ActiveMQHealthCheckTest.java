7bb076877edf4fb89569e6306973692f91e7760c
Iulia Valaczkai <Iulia.Valaczkai@postkodlotteriet.se>
2020-08-06 16:42:59 +0200

    SPL-10600 Fix possible connection leak in the health checker

--- src/test/java/com/kjetland/dropwizard/activemq/ActiveMQHealthCheckTest.java
+++ src/test/java/com/kjetland/dropwizard/activemq/ActiveMQHealthCheckTest.java
@@ -7,7 +7,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.jms.Connection;
+import javax.jms.JMSException;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ActiveMQHealthCheckTest {
 
@@ -35,5 +42,23 @@ public class ActiveMQHealthCheckTest {
         assertEquals(HealthCheck.Result.healthy(), h.check());
         assertEquals(HealthCheck.Result.healthy(), h.check());
         assertEquals(HealthCheck.Result.healthy(), h.check());
+    }
+
+    @Test(expected = JMSException.class)
+    public void testCheckConnectionCloseHandling() throws Exception {
+        //given
+        ActiveMQConnectionFactory connectionFactory = mock(ActiveMQConnectionFactory.class);
+        Connection connection = mock(Connection.class);
+        when(connectionFactory.createConnection()).thenReturn(connection);
+
+        doThrow(new JMSException("JmsError", "999")).when(connection).start();
+
+        ActiveMQHealthCheck h = new ActiveMQHealthCheck(connectionFactory, 3000);
+
+        //when
+        h.check();
+
+        //then
+        verify(connection).close();
     }
 }
